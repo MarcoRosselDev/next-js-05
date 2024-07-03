8fb9df3d6009a2b00f13f903cf900bb89fe52d08
Marco Rossel <andresmarcorossel@gmail.com>
2024-07-03 16:10:07 -0400

    next app/hotel | header with tailwind css

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,38 @@
+'use client'
+
+import Link from "next/link"
+
+export default function Header() {
+  return (
+    <header className="py-10 px-4 container mx-auto text-xl flex flex-wrap md:flex-nowrap items-center justify-between">
+      <div className="flex items-center w-full md:2/3">
+        <Link href='/' className="font-black text-tertiary-dark">
+          Hotelzz
+        </Link>
+        <ul>
+          <li>
+            <Link href='/auth'>
+            auth
+            </Link>
+          </li>
+          <li>
+            <Link href='/'>
+            dark mode
+            </Link>
+          </li>
+        </ul>
+      </div>
+      <ul className="flex items-center justify-between w-full md:w-1/3 mt-4">
+        <li className="hover:translate-y-2 duration-500 transition-all">
+          <Link href='/'>Home</Link>
+        </li>
+        <li className="hover:translate-y-2 duration-500 transition-all">
+          <Link href='/rooms'>Rooms</Link>
+        </li>
+        <li className="hover:translate-y-2 duration-500 transition-all">
+          <Link href='/contact'>Contact</Link>
+        </li>
+      </ul>
+    </header>
+  )
+}
