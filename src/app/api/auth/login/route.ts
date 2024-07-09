d829f147ac44afd5c91bf1e91fa3be2ac2085bb6
Marco Rossel <andresmarcorossel@gmail.com>
2024-07-09 15:17:30 -0400

    next app/hotel | backend | login validations success

--- src/app/api/auth/login/route.ts
+++ src/app/api/auth/login/route.ts
@@ -1,19 +1,35 @@
 import { connectMongoDB } from "@/app/libs/mongodb";
-import { IUser } from "@/models/User";
+import User, { IUser } from "@/models/User";
 import { messages } from "@/utils/mensajes";
 import { NextRequest, NextResponse } from "next/server";
+import bcrypt from "bcryptjs"
+import jwt from "jsonwebtoken" 
 
-export default async function POST(request:NextRequest) {
+export async function POST(request:NextRequest) {
   try {
     await connectMongoDB()
     const body: IUser = await request.json()
     const {email, password} = body
 
     // validamos que se rellenaron los campos
-    if (!email || !password) {
-      return NextResponse.json({message: messages.error.needProps},{status: 400})
-    }
+    if (!email || !password) {return NextResponse.json({message: messages.error.needProps},{status: 400})}
+    const buscar_usuario = await User.findOne({email})
+    if (!buscar_usuario) { return NextResponse.json({message: messages.error.user_not_found},{status: 400})}
+    const comparacion_password: boolean = await bcrypt.compare(password, buscar_usuario.password)
+    // comparacion del password del body con el password del usuario encontrado en mongodb
+    if (!comparacion_password) { return NextResponse.json({message: messages.error.password_mistake},{status: 400})}
+
+    // ahora podemos crear el token si paso todas las validaciones
+    const {password:userPass, ...rest} = buscar_usuario
+    // token
+    const token = jwt.sign({data:rest}, `${process.env.HAS_PASS}`, {expiresIn: 86400})
+    const respuesta = NextResponse.json({
+      userLogged: rest,
+      message: messages.success.usuario_login},{status: 200})
+    respuesta.cookies.set("auth_cookie", token, {sameSite:"strict", path: "/", maxAge: 86400})
+    return respuesta
   } catch (error) {
+    console.log(error);
     
   }
 }
--- src/utils/mensajes.ts
+++ src/utils/mensajes.ts
@@ -4,10 +4,12 @@ export const messages = {
     bad_email_format: "tu email no es valido",
     password_mistake: "las contraseñas no coinsiden",
     email_existente: "ya existe un usuario con ese email",
+    user_not_found: "el usuario no se encuentra registrado",
   },
   success:{
     a: "todo bien",
     usuario_creado: "usuario creado exitosamente",
+    usuario_login:"que bueno tenerte de buelta",
   },
   status:{
     400: "not found ...",
