b1524c6a1e685e2bdbebd759f11dbd7f49d14d16
Marco Rossel <andresmarcorossel@gmail.com>
2025-03-10 23:49:47 -0300

    next reminder - rm ts

---
+++ ts.ts
@@ -0,0 +1,17 @@
+class Peliculas {
+  nombre? : string;
+  protagonistas? : string[];
+  actores? : string[];
+  proyectarEnCine(){
+    console.log(`La pelicula ${this.nombre} esta en cines`);
+  }
+  constructor(nombre:string, protagonistas: string[], actores:string[]){
+    this.nombre = nombre,
+    this.protagonistas = protagonistas,
+    this.actores = actores,
+  }
+}
+
+const pelicula = new Peliculas("Barbie", ["barbie", "ken"], ["margot robien", "alguien mas"])
+
+pelicula.proyectarEnCine()
